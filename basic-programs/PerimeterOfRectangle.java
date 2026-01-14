24e33f3ea2a3d11978a084125e723beaaac31df2
Ujjawal Chaturvedi <ujjawalchaturvedi07@gmail.com>
2026-01-14 19:29:02 +0530

    Added: Program for Calculating Perimeter Of Rectangle

--- basic-programs/PerimeterOfRectangle.java
+++ basic-programs/PerimeterOfRectangle.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class PerimeterOfRectangle {
     public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enter Length of thr Rectangle: ");
-        double l =  sc.nextInt();
+        System.out.println("Enter Length of the Rectangle: ");
+        double l =  sc.nextDouble();
         System.out.println("Enter breadth of the Rectangle: ");
-        double b = sc.nextInt();
+        double b = sc.nextDouble();
         double perimeter = 2*(l+b);
         System.out.println("The Perimeter of Rectangle is: "+perimeter);
         sc.close();
