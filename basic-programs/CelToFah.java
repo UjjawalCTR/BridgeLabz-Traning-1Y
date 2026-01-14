b5651484653c42a9a8e2c4e2148df90e11812f41
Ujjawal Chaturvedi <ujjawalchaturvedi07@gmail.com>
2026-01-14 19:23:24 +0530

    Added: Program for Converting Celsius To Fahrenheit

--- basic-programs/CelToFah.java
+++ basic-programs/CelToFah.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class FahToCel {
+public class CelToFah {
     public static void main(String[] args){
         System.out.println("Enter Reading in Celsius: ");
         Scanner input = new Scanner(System.in);
-        int C = input.nextInt();
-        int Fahrenheit = (C * 9/5) + 32;
+        double C = input.nextDouble();
+        double Fahrenheit = (C * 9/5) + 32;
         System.out.println("After converting into Fahrenheit reading is: " + Fahrenheit);
         input.close();
     }
